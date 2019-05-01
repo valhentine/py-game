3adc07af60798e201d7786a72873e4b5b848f499
valhentine <138431565+valhentine@users.noreply.github.com>
2019-04-30 19:47:51 -0600

    extra stuff added

--- game.py
+++ game.py
@@ -1,22 +1,33 @@
-player = {'name': 'Daniel', 'attack': 10, 'heal': 16, 'health': 100}
+player = {'name': 'Daniel', 'attack': 13, 'heal': 16, 'health': 100}
 monster = {'name': 'Orc', 'attack': 12, 'health': 100}
+game_running = True
+
+while game_running == True:
 
 
-print('Please select action')
-print('1) Attack')
-print('2) Heal')
+    print('Please select action')
+    print('1) Attack')
+    print('2) Heal')
 
-player_choice = input()
+    player_choice = input()
 
 
-if player_choice == '1':
-    monster['health'] = monster['health'] - player['attack']
-    player['health'] = player['health'] - monster['attack']
-    print(monster['health'])
-    print(player['health'])
+    if player_choice == '1':
+        monster['health'] = monster['health'] - player['attack']
+        player['health'] = player['health'] - monster['attack']
+        print(monster['health'])
+        print(player['health'])
 
-elif player_choice == '2':
-    print(player['health'] + player['heal'])
-    
-else:
-    print('Invalid Input')
+    elif player_choice == '2':
+        print(player['health'] + player['heal'])
+
+    else:
+        print('Invalid Input')
+
+    if player['health'] <= 0:
+        game_running = False
+        print('GAME OVER')
+
+    elif monster['health'] <= 0:
+        game_running = False
+        print('YOU WIN')
